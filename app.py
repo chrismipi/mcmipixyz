e8e83b8174668bc39f28ae53d5e4ec6fedf3012e
Chris Mipi <christopher.mipi@gmail.com>
2020-03-24 20:44:15 +0200

    adding some small changes

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -9,7 +11,19 @@ def home():
         years_of_experience='6+',
         age=9
     )
-    
+
+    ip_address = request.remote_addr
+    if ip_address != '127.0.0.1':
+        resp = requests.get('http://ip-api.com/json/' + ip_address)
+
+        resp_json = json.loads(resp.text)
+
+        lon = resp_json['lon']
+        lat = resp_json['lat']
+
+        print('LON ', lon)
+        print('LAT ', lat)
+
     return render_template("home.html", **context)
 
 
